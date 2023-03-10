d63847d60308f57c887fe4a4b5fc3d027ade3833
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-03-10 02:03:05 -0300

    chore: add and configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "styled-components"
+import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes'
