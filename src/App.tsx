0e55d2aa5488487a12d40b780a9e2020c421b144
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-03-10 02:28:51 -0300

    feat: add transactions page and header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes'
 
+import { Transactions } from './pages'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <Transactions />
 
       <GlobalStyle />
     </ThemeProvider>
