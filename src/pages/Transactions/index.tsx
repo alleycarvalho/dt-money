c49e984954ef436322fbcc330ecaaf8a192dd6c9
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-03-12 10:14:03 -0300

    feat: add table transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,38 @@
 import { Header, Summary } from '../../components'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="40%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/01/2023</td>
+            </tr>
+            <tr>
+              <td width="40%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">-R$ 6.000,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>18/01/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
