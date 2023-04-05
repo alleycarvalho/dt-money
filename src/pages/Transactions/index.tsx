f33f540b82b60a5d7683f16761558c8d0534e7f8
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-04-04 22:48:33 -0300

    feat: format values

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 
 import { Header, Summary } from '../../components'
 import { TransactionsContext } from '../../contexts'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from './components'
 import {
   PriceHighlight,
@@ -30,10 +31,13 @@ export function Transactions() {
                 <tr key={id}>
                   <td width="40%">{description}</td>
                   <td>
-                    <PriceHighlight variant={type}>{price}</PriceHighlight>
+                    <PriceHighlight variant={type}>
+                      {type === 'outcome' && '- '}
+                      {priceFormatter.format(price)}
+                    </PriceHighlight>
                   </td>
                   <td>{category}</td>
-                  <td>{createdAt}</td>
+                  <td>{dateFormatter.format(new Date(createdAt))}</td>
                 </tr>
               )
             })}
